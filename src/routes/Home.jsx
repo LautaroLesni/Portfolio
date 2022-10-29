e62377b0ec1f139421f166a820505d7a51681340
LautaroLesni <lautarolesniewicz@gmail.com>
2022-10-29 06:46:15 -0300

    agregado mensaje

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,6 +6,9 @@ const Home = () =>{
         <div>
             <Navbar/>
             <h1>Home</h1>
+            <h1>Mi nombre es Lautaro Lesniewicz</h1>
+            <h3>Si estas leyendo esto es porque mi portfolio esta en proceso.
+            Dentro de muy poco voy a tenerlo listo para que lo veas!</h3>
         </div>
     )
 }
