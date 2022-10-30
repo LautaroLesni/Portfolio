a914a126c09d77c01f93e6b914d8eba5dd6ae969
LautaroLesni <lautarolesniewicz@gmail.com>
2022-10-29 21:39:18 -0300

    more changes

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,25 +1,12 @@
 import React from "react";
 import Navbar from '../components/Navbar/Navbar'
-import lautaroimg from '../assets/Lautaro1.png'
-import './Home.css'
-import { Link } from "react-router-dom";
+import Intro from '../components/Home/Intro'
 
 const Home = () => {
     return (
         <div>
             <Navbar />
-            <div className="hero">
-                <div className="lautaroimgdiv">
-                    <img src={lautaroimg} alt='Lautaro' className="lautaroimg"></img>
-                </div>
-                <div className="content">
-                    <p>I'm Lautaro Lesniewicz</p>
-                    <h1>Fullstack Developer</h1>
-                    <div className="buttons">
-                        <Link to='/about' className="button-62">Conoceme</Link>
-                    </div>
-                </div>
-            </div>
+            <Intro/>
             <h3>Si estas leyendo esto es porque mi portfolio esta en proceso.
                 Dentro de muy poco voy a tenerlo listo para que lo veas!</h3>
         </div>
