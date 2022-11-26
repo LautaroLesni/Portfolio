4c6f41aec57082b8573643e7aca82bcc98612c85
LautaroLesni <lautarolesniewicz@gmail.com>
2022-11-26 19:23:52 -0300

    fix scroll issue

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -3,9 +3,14 @@ import Navbar from '../components/Navbar/Navbar'
 import AboutInfo from '../components/About/AboutInfo'
 import Skills from '../components/About/Skills'
 import Footer from '../components/Footer/Footer'
+import { useEffect } from "react";
 
 
 const About = () =>{
+
+    useEffect(()=>{
+        window.scrollTo(0,0)
+    })
     return (
         <div>
             <Navbar />
